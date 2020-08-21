c8c4418fa4e97ea46c186536e79b6fc8ffedd969
Olga Nilova <nila2991@gmail.com>
2020-08-21 20:03:47 +0200

    second commit

--- Simple_Banking_System.py
+++ Simple_Banking_System.py
@@ -2,21 +2,38 @@ import random
 
 class Card:
   def __init__(self):
-    self.number = None
-    self.pin = None
+    self.number = self.new_card_number()
+    self.pin = self.new_card_pin()
     self.balance = 0
 
+  def new_card_number(self):
+    account_number = str(random.randint(000000000, 999999999))
+    if len(account_number) < 9:
+      account_number = '0' * (9 - len(account_number)) + account_number
+    account_number = '400000' + account_number
+    return account_number + self.luhn_alg(account_number)
 
-  def new_card(self):
-    account_number = str(random.randint(0000000000, 9999999999))
-    if len(account_number) < 10:
-      account_number = '0' * (10 - len(account_number)) + account_number
-    self.number = '400000' + account_number
+  def luhn_alg(self, number):
+    summ = 0
+    for i in range(1, 16):
+      if i % 2 == 1:
+        digit = int(number[i-1]) * 2
+        if digit > 9:
+          digit -= 9
+        summ += digit
+      else:
+        summ += int(number[i-1])
+    if summ % 10 == 0:
+      checksumm = 0
+    else:
+      checksumm = 10 - summ % 10
+    return str(checksumm)
 
-    self.pin = str(random.randint(0000, 9999))
-    if len(self.pin) < 4:
-      self.pin = '0' * (4 - len(self.pin)) + self.pin
-
+  def new_card_pin(self):
+    account_pin = str(random.randint(0000, 9999))
+    if len(account_pin) < 4:
+      account_pin = '0' * (4 - len(account_pin)) + account_pin
+    return account_pin
 
   def check_inf(self, number, pin):
     if self.number != number or self.pin != pin:
@@ -25,19 +42,18 @@ class Card:
       print('\nYou have successfully logged in!\n')
       return True
 
-
   def balances(self):
     print('\nBalance: ', self.balance, '\n')
 
 
 
 client_choice = int(input('1. Create an account\n2. Log into account\n0. Exit\n'))
-
+cards = []
 while client_choice != 0:
   
   if client_choice == 1:
     client_card = Card()
-    client_card.new_card()
+    cards.append(client_card)
     print('\nYour card has been created')
     print('Your card number:')
     print(client_card.number)
@@ -62,4 +78,4 @@ while client_choice != 0:
 
   client_choice = int(input('1. Create an account\n2. Log into account\n0. Exit\n'))
 
-print('\nBye!') 
+print('\nBye!')
