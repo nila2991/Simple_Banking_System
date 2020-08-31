5845315cd7067b43d8f95ab8388988e3cbbd84f2
Olga Nilova <nila2991@gmail.com>
2020-08-31 20:53:20 +0200

    final comment

--- Simple_Banking_System.py
+++ Simple_Banking_System.py
@@ -1,19 +1,30 @@
 import random
+import sqlite3
 
 class Card:
   def __init__(self):
+    self.id = self.new_card_id()
     self.number = self.new_card_number()
     self.pin = self.new_card_pin()
-    self.balance = 0
+
+  def new_card_id(self):
+    account_id = str(random.randint(000000000, 999999999))
+    if len(account_id) < 9:
+      account_id = '0' * (9 - len(account_id)) + account_id
+    return account_id
 
   def new_card_number(self):
-    account_number = str(random.randint(000000000, 999999999))
-    if len(account_number) < 9:
-      account_number = '0' * (9 - len(account_number)) + account_number
-    account_number = '400000' + account_number
-    return account_number + self.luhn_alg(account_number)
+    account_number = '400000' + self.id
+    return account_number + luhn_alg(account_number)
 
-  def luhn_alg(self, number):
+  def new_card_pin(self):
+    account_pin = str(random.randint(0000, 9999))
+    if len(account_pin) < 4:
+      account_pin = '0' * (4 - len(account_pin)) + account_pin
+    return account_pin
+
+
+def luhn_alg(number):
     summ = 0
     for i in range(1, 16):
       if i % 2 == 1:
@@ -29,31 +40,79 @@ class Card:
       checksumm = 10 - summ % 10
     return str(checksumm)
 
-  def new_card_pin(self):
-    account_pin = str(random.randint(0000, 9999))
-    if len(account_pin) < 4:
-      account_pin = '0' * (4 - len(account_pin)) + account_pin
-    return account_pin
+def check_inf(number, pin):
+  if number not in all_client_cards or all_client_cards[number] != pin:
+    print('\nWrong card number or PIN!\n')
+  else:
+    print('\nYou have successfully logged in!\n')
+    return True
 
-  def check_inf(self, number, pin):
-    if self.number != number or self.pin != pin:
-      print('\nWrong card number or PIN!\n')
+def client_balance(number):
+  cur.execute('SELECT * FROM card WHERE number = ?;', (number, ))
+  conn.commit()
+  r = cur.fetchone()
+  return r[3]
+
+def client_income(number, income):
+  cur.execute('UPDATE card SET balance = balance + ? WHERE number = ?;', (income, number))
+  conn.commit()
+
+def client_decline(number, summ):
+  cur.execute('UPDATE card SET balance = balance - ? WHERE number = ?;', (summ, number))
+  conn.commit()
+
+def transfer(number_from, number_to):
+  cur.execute('SELECT * FROM card WHERE number = ?;', (number_to, ))
+  conn.commit()
+  existence = cur.fetchall()
+  if number_from == number_to:
+    print("You can't transfer money to the same account!\n")
+  elif luhn_alg(number_to[0:15]) != number_to[15]:
+    print('Probably you made a mistake in the card number. Please try again!')
+  elif len(existence) == 0:
+    print('Such a card does not exist.')
+  else:
+    print('Enter how much money you want to transfer:')
+    money = int(input())
+    if client_balance(number_from) < money:
+      print('Not enough money!')
     else:
-      print('\nYou have successfully logged in!\n')
-      return True
+      print('Success!')
+      client_income(number_to, money)
+      client_decline(number_from, money)
 
-  def balances(self):
-    print('\nBalance: ', self.balance, '\n')
+def close_account(number):
+  cur.execute('DELETE FROM card WHERE number = ?;', (number, ))
+  conn.commit()
+  del all_client_cards[number]
 
 
+conn = sqlite3.connect('card.s3db')
+cur = conn.cursor()
 
+#cur.execute('DROP TABLE card')
+cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER PRIMARY KEY, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);')
+conn.commit()
 client_choice = int(input('1. Create an account\n2. Log into account\n0. Exit\n'))
-cards = []
+all_client_cards = {}
 while client_choice != 0:
-  
+
   if client_choice == 1:
     client_card = Card()
-    cards.append(client_card)
+    cur.execute('SELECT * FROM card WHERE number = ?;', (client_card.number, ))
+    conn.commit()
+    rows = cur.fetchall()
+    while len(rows) != 0:
+      client_card = Card()
+      cur.execute('SELECT count(*) FROM card WHERE number = ?;', (client_card.number, ))
+      conn.commit()
+      rows = cur.fetchall()
+    all_client_cards[client_card.number] = client_card.pin
+    cur.execute('INSERT INTO card (number, pin) VALUES (?, ?)', (client_card.number, client_card.pin))
+    conn.commit()
+    #cur.execute('SELECT id, number, pin, balance FROM card')
+    #print(cur.fetchall())
+    #cards.append(client_card)
     print('\nYour card has been created')
     print('Your card number:')
     print(client_card.number)
@@ -64,17 +123,36 @@ while client_choice != 0:
   elif client_choice == 2:
     card_number = input('\nEnter your card number:\n')
     card_pin = input('Enter your PIN:\n')
-    if client_card.check_inf(card_number, card_pin) == True:
-      client_balance = input('1. Balance\n2. Log out\n0. Exit\n')
-      if client_balance == '0':
-        break
-      while True:
-        if client_balance == '1':
-          client_card.balances()
-        elif client_balance == '2':
+    if check_inf(card_number, card_pin) == True:
+      client_act = input('1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n')
+
+      while client_act != '0':
+        if client_act == '1':
+          print('\nBalance: ', client_balance(card_number))
+
+        elif client_act == '2':
+          print('\nEnter income:')
+          income = int(input())
+          client_income(card_number, income)
+          print('Income was added!')
+
+        elif client_act == '3':
+          print('\nTransfer\nEnter card number:')
+          number_to = input()
+          transfer(card_number, number_to)
+
+        elif client_act == '4':
+          print('\nThe account has been closed!\n')
+          close_account(card_number)
+          break
+
+        elif client_act == '5':
           print('\nYou have successfully logged out!\n')
           break
-        client_balance = input('1. Balance\n2. Log out\n0. Exit\n')
+
+        client_act = input('\n1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n')
+      if client_act == '0':
+        break
 
   client_choice = int(input('1. Create an account\n2. Log into account\n0. Exit\n'))
 
